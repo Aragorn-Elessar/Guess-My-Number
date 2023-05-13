5d2d508e2afd7ca4cee7354a19fd083348b13c17
Aragorn Elessar <mahmoud_gadallah@outlook.com>
2023-05-13 03:14:09 +0300

    Add event handler for check click

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Handle user guessed number input
+document.querySelector('.check').addEventListener('click', function () {
+  // Grab the guessed number
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // Notify user if a zero value or no number entered
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  }
+});
