99cd8b7e7d60f0326af36d48e4dfb1c2d1dd481c
Aragorn Elessar <mahmoud_gadallah@outlook.com>
2023-05-17 22:55:08 +0300

    Implement Highscore

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 let randomNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = '';
 
 // Handle user guessed number input
 document.querySelector('.check').addEventListener('click', function () {
@@ -19,6 +20,12 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    // Update highscore
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // When guess is too high
   } else if (guess > randomNumber && score > 1) {
